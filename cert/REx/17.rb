dc288486efbf3bb01d0108ddaeee39845a94b1bd
neu-k <rsk367277@gmail.com>
2024-10-13 07:00:16 +0900

    instance_eval, module_eval

--- cert/REx/17.rb
+++ cert/REx/17.rb
@@ -6,6 +6,7 @@ _proc = Proc.new do
   CONST = "Constant in Proc"
 end
 
+# instance_evelに文字列を渡しているため、スコープはm＝モジュール定義と同じになる
 m.instance_eval(<<-EOS)
   CONST = "Constant in Module instance"
 
@@ -14,7 +15,8 @@ m.instance_eval(<<-EOS)
   end
 EOS
 
-m.module_eval(&_proc)
 # module_evalにブロックを渡しているため、スコープはこの箇所＝トップレベルになり再定義の警告が出る
+m.module_eval(&_proc)
 
-p m.const   # つまり、定数探索に関する問題
+
+p m.const
